bb33b08cb50f415386a76f77f95daaa427b14cf5
asmaashreit <asmaashreit11@gmail.com>
2021-01-07 02:35:02 +0200

    first version

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,16 +61,91 @@ asideSetting.addEventListener('click', () => {
     asideSetting.classList.toggle('active');
 });
 
+// Counter enrolled 
+$(document).ready(function(){
+    $('.enrolled .counter').counterUp({
+        delay: 10,
+        time: 1200
+    });
+});
 
+// Slide courses
+const carouselTrack = document.querySelector('.carousel__track');
+const slides = Array.from(carouselTrack.children);
+const nextBtn = document.querySelector('.carousel__button--right');
+const prevBtn = document.querySelector('.carousel__button--left');
+
+// console.log(slides[0].getBoundingClientRect());
+const sizeGap = parseInt(window.getComputedStyle(carouselTrack).getPropertyValue('gap'));
+const slideWidth = slides[0].getBoundingClientRect().width+sizeGap;
+slides[0].classList.add('active');
+
+const contentCourse = document.querySelectorAll('.slide-courses .carousel__slide--item');
+const cardCourses = document.querySelectorAll('.slide-courses .card');
+const moreDetails = document.querySelectorAll('.slide-courses .more-details');
+
+
+cardCourses.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        //console.log(card.parentElement.getBoundingClientRect());
+        if(card.parentElement.classList.contains('active')) {
+            moreDetails.forEach(details => {
+                if(details.parentElement === card.parentElement) {
+                    details.style.transform = 'translateX(100%)';
+                    details.style.right = '-0.5rem';
+                    details.style.left = 'auto';
+                }
+            }) 
+        }else {  
+            moreDetails.forEach(details => {
+                if(details.parentElement === card.parentElement) {
+                    details.style.transform = 'translateX(-100%)';
+                    details.style.left = '-0.5rem';
+                    details.style.right = 'auto';
+                    
+                }
+            }) 
+        }
+    })
+})
+
+nextBtn.addEventListener('click', () => {
+    const currentSlide = carouselTrack.querySelector('.active');
+    const nextSlide = currentSlide.nextElementSibling;
+    currentSlide.classList.remove('active');
+    nextSlide.classList.add('active');
+    //nextSlide.style.left = 'slideWidth';
+    carouselTrack.style.transform = `translateX(-${slideWidth * slides.indexOf(nextSlide)}px)`;
+    prevBtn.style.display = 'block';
+    if(slides.indexOf(nextSlide) >= slides.length-1){
+        nextBtn.style.display = 'none';
+    }
+});
+
+prevBtn.addEventListener('click', () => {
+    const currentSlide = carouselTrack.querySelector('.active');
+    const prevSlide = currentSlide.previousElementSibling;
+    currentSlide.classList.remove('active');
+    prevSlide.classList.add('active');
+    carouselTrack.style.transform = `translateX(-${slideWidth * slides.indexOf(prevSlide)}px)`;
+    nextBtn.style.display = 'block';
+    if(slides.indexOf(prevSlide) == 0){
+        prevBtn.style.display = 'none';
+    }
+});    
 
 // Change accordion icon 
 const accordion = document.querySelectorAll('.card-header');
 accordion.forEach(accord => {
     accord.addEventListener('click', () => {
-        accordion.forEach(accord => {
-            accord.classList.remove('active');
-        });
-        accord.classList.add('active');
+        if(accord.classList.contains('active')){accord.classList.remove('active');
+        }else{
+            accordion.forEach(accord => {
+                accord.classList.remove('active');
+            });
+            accord.classList.add('active');
+        }
+        
     });
 });
 // carousel delay
@@ -82,7 +157,7 @@ accordion.forEach(accord => {
 AOS.init();
 
 // Owl carousel (all courses)
-$('.all-courses .owl-carousel').owlCarousel({
+/*$('.all-courses .owl-carousel').owlCarousel({
     loop:true,
     margin:15,
     responsiveClass:true,
@@ -108,37 +183,9 @@ $('.all-courses .owl-carousel').owlCarousel({
             loop:true
         }
     }
-});
+});*/
 
-const contentCourse = document.querySelectorAll('.all-courses .content-course');
-const cardCourses = document.querySelectorAll('.all-courses .card');
-const moreDetails = document.querySelectorAll('.all-courses .more-details');
 
-contentCourse.forEach(content => {
-    content.addEventListener('mouseover', (e) => {
-        
-        
-    })
-})
-
-cardCourses.forEach(card => {
-    card.addEventListener('mouseover', () => {
-        moreDetails.forEach(details => {
-            //if(content.getBoundingClientRect().left <= content.offsetWidth) {
-            //console.log(details.parentElement);
-            if(details.parentElement === card.parentElement && details.parentElement.getBoundingClientRect().left < details.parentElement.clientWidth) {
-                console.log(details.parentElement.getBoundingClientRect().left);
-                console.log(details.parentElement.clientWidth);
-                console.log(details.parentElement.offsetWidth);
-                details.style.direction = 'rlt';
-                details.style.transfrom = 'translateX(100%)';
-                details.style.right = '1rem';
-            }
-            
-            //}
-        })    
-    })
-})
 
 
 
